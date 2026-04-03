c780c96ca74799133da768399bca17bfb8f90ece
sarangabani1984 <sarangabani1984@gmail.com>
2026-04-03 14:55:09 +0530

    aggr funcFind departments with average salary > ,000 AND more than 2 employeestion

--- L2_AGGR_COMPLETE_My_Answer.sql
+++ L2_AGGR_COMPLETE_My_Answer.sql
@@ -1,31 +1,30 @@
-
-
-use InterviewL1_Joins
-
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES;
 
 SELECT 
-customer_name,
-o.order_id,
-p.product_name,
-oi.quantity
-FROM Customers c
-JOIN Orders o
-ON c.customer_id = o.customer_id
-JOIN Order_Items oi
-on oi.order_id = o.order_id
-JOIN Products p
-on p.product_id = oi.product_id;
+d.dept_name,
+avg(e.salary) as avg_salary,
+COUNT(e.emp_id) as employee_count
+FROM Departments d
+LEFT JOIN Employees e
+ON d.dept_id = e.dept_id
+GROUP BY d.dept_name
+ORDER BY avg_salary DESC;
 
+
+-- Q5.2: Find departments with average salary > $100,000 AND more than 2 employees
 
 SELECT 
-    C.customer_name,
-    SUM(o.amount) AS total_spent,
-    C.city                          -- ← Add this
-FROM Customers AS C
-JOIN Orders AS O
-ON C.customer_id = O.customer_id
-WHERE C.country = 'USA'
-GROUP BY C.customer_name, C.city    -- ← Add city here too
-HAVING SUM(o.amount) > 200;
+d.dept_name,
+avg(e.salary) as avg_salary,
+COUNT(e.emp_id) as employee_count
+FROM Departments d
+LEFT JOIN Employees e
+ON d.dept_id = e.dept_id
+GROUP BY d.dept_name
+HAVING avg(e.salary) > 100000 AND COUNT(e.emp_id) > 2
+ORDER BY avg_salary DESC;
+
+
+
+
